c0f7ccb61a460e5533839ee58e0325d8695bb42c
Lucas Souza <souzadev2025@gmail.com>
2026-09-15 18:58:43 -0300

    Refactor chat_whit_tools function and update model

--- service.py
+++ service.py
@@ -23,7 +23,7 @@ def chat_whit_tools(mensagem):
 
     client = Groq(api_key = os.environ.get('CHAVE_KEY'))   
     response = client.chat.completions.create(
-    model="llama-3.1-8b-instant",
+    model="llama3-8b-8192",
     messages=[
         {
             "role": "system",
